6a24b3d9e603d6276e1d3a5942f5d76f48c936ee
denusilchuk9-cell <denusilchuk9@gmail.com>
2026-03-02 23:11:15 +0200

    Add files via upload

---
+++ Business/GradeCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ElectronicJournal.Models;
+
+namespace ElectronicJournal.Business
+{
+    public class GradeCalculator
+    {
+        public double CalculateAverage(List<Grade> grades)
+        {
+            if (grades == null || grades.Count == 0)
+                return 0;
+
+            return grades.Average(g => g.Value);
+        }
+
+        public double CalculateAttendancePercentage(List<Attendance> attendances)
+        {
+            if (attendances == null || attendances.Count == 0)
+                return 0;
+
+            int present = attendances.Count(a => a.IsPresent);
+            return (double)present / attendances.Count * 100;
+        }
+    }
+}
