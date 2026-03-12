581042b101e9f916e49302fbfce1a05efd4bcad8
denusilchuk9-cell <denusilchuk9@gmail.com>
2026-03-13 01:35:17 +0200

    Add files via upload

---
+++ Interfaces/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ElectronicJournal.Models;
+
+namespace ElectronicJournal.Interfaces
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+        List<Student> GetByGroup(string group);
+        Student GetByRecordBook(string recordBook);
+    }
+}
